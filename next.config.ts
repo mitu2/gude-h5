cfe9155e6558457beff2d80929e6664332787011
mitu2 <chenmoand@outlook.com>
2025-08-18 10:22:47 +0800

    feat(deployment): 添加 Cloudflare Pages 部署支持

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type {NextConfig} from "next";
+import {setupDevPlatform} from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
     /* 基础配置 */
@@ -42,5 +43,9 @@ const nextConfig: NextConfig = {
     // },
 
 };
+
+if (process.env.NODE_ENV === "development") {
+    await setupDevPlatform();
+}
 
 export default nextConfig;
